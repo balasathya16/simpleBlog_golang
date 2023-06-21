0039368208de119d3d613ae8d84b5a92e12f906d
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-21 18:40:07 -0400

    implemented create, update and delete blog funcs

--- backend/controllers/blog_controller.go
+++ backend/controllers/blog_controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
+	"simpleBlog/backend/models"
 	"simpleBlog/backend/repositories"
 
 	"github.com/gorilla/mux"
@@ -58,13 +60,103 @@ func (c *BlogController) GetBlogByID(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *BlogController) CreateBlog(w http.ResponseWriter, r *http.Request) {
-	// Implementation for creating a new blog
+	// Parse the request body to extract the blog data
+	var blog models.Blog
+	err := json.NewDecoder(r.Body).Decode(&blog)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the blog data
+	if err := validateBlog(&blog); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Call the repository method to create the blog
+	err = c.repo.Create(&blog)
+	if err != nil {
+		http.Error(w, "Failed to create blog", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response status and content type
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
+	// Send the created blog as the response body
+	json.NewEncoder(w).Encode(blog)
+}
+
+func validateBlog(blog *models.Blog) error {
+	// Implement your validation logic here
+	// For example, you can check if the required fields are present
+	if blog.Title == "" || blog.Content == "" || blog.Author == "" {
+		return errors.New("required fields are missing")
+	}
+
+	return nil
 }
 
 func (c *BlogController) UpdateBlog(w http.ResponseWriter, r *http.Request) {
-	// Implementation for updating an existing blog
+	// Parse the blog ID from the request URL
+	vars := mux.Vars(r)
+	blogID := vars["id"]
+
+	// Parse the request body to extract the updated blog data
+	var updatedBlog models.Blog
+	err := json.NewDecoder(r.Body).Decode(&updatedBlog)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the updated blog data
+	if err := validateBlog(&updatedBlog); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the existing blog from the repository
+	existingBlog, err := c.repo.GetByID(blogID)
+	if err != nil {
+		http.Error(w, "Blog not found", http.StatusNotFound)
+		return
+	}
+
+	// Update the fields of the existing blog with the updated data
+	existingBlog.Title = updatedBlog.Title
+	existingBlog.Content = updatedBlog.Content
+	existingBlog.Author = updatedBlog.Author
+
+	// Call the repository method to update the blog
+	err = c.repo.Update(existingBlog)
+	if err != nil {
+		http.Error(w, "Failed to update blog", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response status and content type
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	// Send the updated blog as the response body
+	json.NewEncoder(w).Encode(existingBlog)
 }
 
 func (c *BlogController) DeleteBlog(w http.ResponseWriter, r *http.Request) {
-	// Implementation for deleting a blog
+	// Parse the blog ID from the request URL
+	vars := mux.Vars(r)
+	blogID := vars["id"]
+
+	// Call the repository method to delete the blog
+	err := c.repo.Delete(blogID)
+	if err != nil {
+		http.Error(w, "Failed to delete blog", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response status
+	w.WriteHeader(http.StatusOK)
 }

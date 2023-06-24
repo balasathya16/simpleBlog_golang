7a42c54d9efbdd35bcef0acc41643fd2d62b2015
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-24 01:50:01 -0400

    created directory structure for frontend

---
+++ frontend/src/components/BlogList.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import API_BASE_URL from './config.js';
+
+function BlogList() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/blogs`);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return <div>Blog List</div>;
+}
+
+export default BlogList;

e603b735d048c9c72601d1974c2695088c290860
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-25 14:23:29 -0400

    added home page along with the routes

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Blog!</h1>
+      <p>Here you will find insightful articles on various topics.</p>
+      <p>Feel free to explore the blogs section to read the latest posts.</p>
+    </div>
+  );
+};
+
+export default Home;

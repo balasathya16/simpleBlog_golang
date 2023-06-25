375ac586eff17a248ba7e6c4d9829b73ee99c700
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-06-25 15:24:01 -0400

    fixed header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/blogs">Blogs</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+    <header className="header">
+  <h1 className="title">A Failed Cricketer</h1>
+  <div className="spacer"></div>
+  <div className="button-container">
+    <ul className="nav-list">
+      <li className="nav-item"><a href="/blogs">Blogs</a></li>
+    </ul>
+    <button className="login-button">Login</button>
+    <button className="start-button">Get Started</button>
+  </div>
+  <div className="spacer"></div>
+</header>
+  
   );
 };
 
